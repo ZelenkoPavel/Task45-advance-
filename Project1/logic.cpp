07c146d494dcc871dc0932ad4786445440073721
tokio220506.pavel@gmail.com <tokio220506.pavel@gmail.com>
2024-03-26 13:39:36 +0300

    Task

--- Project1/logic.cpp
+++ Project1/logic.cpp
@@ -1,13 +1,47 @@
 
 
 void logic(int* vector_1, int* vector_2, int* vector_3, int size_1, int size_2) {
+	
+	int min = 2147483647;
+	int max = -2147483648;
 
-	for (int i = 0; i < size_1; i++)
-	{
-		*(vector_3 + i) = *(vector_1 + i);
+	for (int i = 0; i < size_1; i++) {
+		if (*(vector_1 + i) > max) {
+			max = vector_1[i];
+		}
 	}
-	for (int j = 0; j < size_2; j++)
+	for (int j = 0; j < size_1; j++) {
+		if (*(vector_1 + j) < min) {
+			min = vector_1[j];
+		}
+	}
+
+	for (int i = 0; i < size_2; i++) {
+		if (*(vector_2 + i) > max) {
+			max = vector_2[i];
+		}
+	}
+	for (int j = 0; j < size_2; j++) {
+		if (*(vector_2 + j) < min) {
+			min = vector_1[2];
+		}
+	}
+
+	int index = 0;
+
+	for (int k = min; k <= max; k++)
 	{
-		*(vector_3 + j + size_1) = *(vector_2 + j);
+		for (int i = 0; i < size_1; i++) {
+			if (*(vector_1 + i) == k) {
+				*(vector_3 + index) = *(vector_1 + i);
+				index++;
+			}
+		}
+		for (int j = 0; j < size_2; j++) {
+			if (*(vector_2 + j) == k) {
+				*(vector_3 + index) = *(vector_2 + j);
+				index++;
+			}
+		}
 	}
 }
